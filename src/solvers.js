4f94c3e3f6393746aef9ae4a511a90f5cf95c134
armandopmj <armandopmj@gmail.com>
2014-07-31 16:02:36 -0700

    Draft Unfinished Solution

--- src/solvers.js
+++ src/solvers.js
@@ -14,10 +14,60 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var n = n, outcomes = [], filteredOutcomes = [], matricies = []; allzeros=[];
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  for( var p = 0; p < n; p++ ) {
+    var row = Array(n);
+    for( var i = 0; i < n; i++ ){
+      row[i] = 0;
+    }
+    row[p] = 1;
+    filteredOutcomes.push( row );
+  }
+
+  for( var p = 0; p < n; p++ ) {
+    allzeros.push(0)
+  }
+  // console.log("allzeros: ", allzeros)
+  var counter = 0;
+  var getMatrix = function(playedSoFar, roundsLeft) {
+    if (roundsLeft === 0) matricies.push( playedSoFar );
+    else {
+      for (var i = 0; i < filteredOutcomes.length; i++) {
+        var building = playedSoFar.concat( [ filteredOutcomes[i] ] );
+        // console.log("building: ", building);
+        if( building.length < n) {
+          var semi_built = [];
+          console.log( "building: ", building )
+          console.log( "n - building.length: ", n - building.length )
+          for( var i = 0; i < n - building.length; i++ ){
+            semi_built = building.concat( [ allzeros ] );
+          }
+          console.log( "semi_built: ", semi_built );
+          var board = new Board( semi_built );
+          if( board.hasAnyRooksConflicts() ) continue;
+        }
+        else {
+          var board = new Board( building );
+          if( board.hasAnyRooksConflicts() ) continue;
+        }
+        getMatrix( building, roundsLeft-1 )
+      counter++;
+      if (counter === 300) debugger;
+      }
+    }
+  };
+  getMatrix([], n);
+  console.log("Matrix ", matricies);
+  console.log("Number of solutions for n of ", n, ": ", matricies.length );
+  return matricies[0];
+  // for( var i = 0; i < matricies.length; i++ ){
+  //   var board = new Board(matricies[i]);
+  //   if( !board.hasAnyRooksConflicts() ) {
+  //     console.log('Single solution for ' + n + ' rooks:', JSON.stringify(matricies[i]));
+  //     return matricies[i];
+  //   }
+  // }
 };
 
 
